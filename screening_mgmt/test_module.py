b6b5368e272725496ef36efb33ee5f6fd07698fc
Andreas Helfenstein <andreas.helfenstein@hotmail.ch>
2016-02-12 13:16:40 +0200

    added more tests

--- screening_mgmt/test_module.py
+++ screening_mgmt/test_module.py
@@ -3,6 +3,7 @@ Last updated 08.02.2016 Andreas Helfenstein
 """
 
 import screening_mgmt as sm
+import aux_func as af
 reload(sm)
 
 def test_connect():
@@ -26,3 +27,35 @@ def test_new_entry():
     db.new_entry(sm.Usr, data)
     user_name = db.session.query(sm.Usr.usr_name,).all()
     assert user_name[0][0] == 'testname'
+    
+# Testing auxiliary functions
+
+def test_sm_shorten_name():
+    name_pairs = (
+        ('Escherichia coli','E. coli'),
+        ('Escherichia','Escherichia'),
+        ('escherichia Coli','E. coli'),
+        )
+    for pair in name_pairs:
+        assert af.sm_shorten_name(pair[0]) == pair[1]
+
+def test_sm_quality():
+    vals = af.sm_quality(1,0.1,0.1,0.1)
+    assert vals['S2B'] == 10
+    assert 6.3 < vals['S2N'] <6.4
+    assert 0.33 < vals['Z'] < 0.34
+    
+def test_Cpd():
+    cpd = sm.Cpd(None)
+    assert cpd.__tablename__ == "compounds"
+    
+def test_Usr():
+    usr = sm.Usr(None)
+    assert usr.__tablename__ == "users"
+    
+def test_Res():
+    res = sm.Res(None)
+    assert res.__tablename__ == "results"
+
+if __name__ == "__main__":
+    test_Cpd()
